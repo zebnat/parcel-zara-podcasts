70f23fe130d3a58a90380269350e6daa1375fafd
Daniel DR <contacto@danieldev.es>
2023-04-05 21:44:57 +0200

    chore: parcel, react, typescript, tailwind bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,135 @@
+import React from "react";
+
+import { StatusIndicator } from "./components/StatusIndicator";
+
+function App() {
+  return (
+    <div className="bg-gray-100 text-black px-10 py-5 min-h-screen">
+      <div className="w-full xl:w-3/4 m-auto flex flex-col">
+        <header className="flex justify-between">
+          <a className="text-blue-500 text-2xl font-bold" href="/">
+            Podcaster
+          </a>
+          <StatusIndicator loading={true} />
+        </header>
+        <main className="my-10 grow">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ante
+            iaculis, euismod leo at, congue mi. Fusce ut lorem rutrum, placerat
+            ligula nec, tempus tellus. Donec fermentum sem sit amet ante cursus,
+            vitae scelerisque elit dictum. Suspendisse lectus nibh, eleifend
+            ultrices molestie at, mollis ullamcorper turpis. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Suspendisse sem lectus,
+            egestas ut lectus sit amet, convallis feugiat leo. Sed luctus
+            iaculis lorem et interdum. Nam ac lobortis justo. Aliquam vestibulum
+            purus vitae lacus condimentum congue. Ut tincidunt tempus euismod.
+            Aliquam felis enim, luctus eu pharetra elementum, rutrum a justo.
+            Nunc pulvinar purus luctus tellus blandit, non blandit dui
+            fermentum. Proin non suscipit felis. Ut at nibh pharetra,
+            sollicitudin quam eget, maximus ex. Praesent laoreet pretium leo,
+            vel bibendum eros euismod accumsan.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ante
+            iaculis, euismod leo at, congue mi. Fusce ut lorem rutrum, placerat
+            ligula nec, tempus tellus. Donec fermentum sem sit amet ante cursus,
+            vitae scelerisque elit dictum. Suspendisse lectus nibh, eleifend
+            ultrices molestie at, mollis ullamcorper turpis. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Suspendisse sem lectus,
+            egestas ut lectus sit amet, convallis feugiat leo. Sed luctus
+            iaculis lorem et interdum. Nam ac lobortis justo. Aliquam vestibulum
+            purus vitae lacus condimentum congue. Ut tincidunt tempus euismod.
+            Aliquam felis enim, luctus eu pharetra elementum, rutrum a justo.
+            Nunc pulvinar purus luctus tellus blandit, non blandit dui
+            fermentum. Proin non suscipit felis. Ut at nibh pharetra,
+            sollicitudin quam eget, maximus ex. Praesent laoreet pretium leo,
+            vel bibendum eros euismod accumsan.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ante
+            iaculis, euismod leo at, congue mi. Fusce ut lorem rutrum, placerat
+            ligula nec, tempus tellus. Donec fermentum sem sit amet ante cursus,
+            vitae scelerisque elit dictum. Suspendisse lectus nibh, eleifend
+            ultrices molestie at, mollis ullamcorper turpis. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Suspendisse sem lectus,
+            egestas ut lectus sit amet, convallis feugiat leo. Sed luctus
+            iaculis lorem et interdum. Nam ac lobortis justo. Aliquam vestibulum
+            purus vitae lacus condimentum congue. Ut tincidunt tempus euismod.
+            Aliquam felis enim, luctus eu pharetra elementum, rutrum a justo.
+            Nunc pulvinar purus luctus tellus blandit, non blandit dui
+            fermentum. Proin non suscipit felis. Ut at nibh pharetra,
+            sollicitudin quam eget, maximus ex. Praesent laoreet pretium leo,
+            vel bibendum eros euismod accumsan.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ante
+            iaculis, euismod leo at, congue mi. Fusce ut lorem rutrum, placerat
+            ligula nec, tempus tellus. Donec fermentum sem sit amet ante cursus,
+            vitae scelerisque elit dictum. Suspendisse lectus nibh, eleifend
+            ultrices molestie at, mollis ullamcorper turpis. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Suspendisse sem lectus,
+            egestas ut lectus sit amet, convallis feugiat leo. Sed luctus
+            iaculis lorem et interdum. Nam ac lobortis justo. Aliquam vestibulum
+            purus vitae lacus condimentum congue. Ut tincidunt tempus euismod.
+            Aliquam felis enim, luctus eu pharetra elementum, rutrum a justo.
+            Nunc pulvinar purus luctus tellus blandit, non blandit dui
+            fermentum. Proin non suscipit felis. Ut at nibh pharetra,
+            sollicitudin quam eget, maximus ex. Praesent laoreet pretium leo,
+            vel bibendum eros euismod accumsan.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ante
+            iaculis, euismod leo at, congue mi. Fusce ut lorem rutrum, placerat
+            ligula nec, tempus tellus. Donec fermentum sem sit amet ante cursus,
+            vitae scelerisque elit dictum. Suspendisse lectus nibh, eleifend
+            ultrices molestie at, mollis ullamcorper turpis. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Suspendisse sem lectus,
+            egestas ut lectus sit amet, convallis feugiat leo. Sed luctus
+            iaculis lorem et interdum. Nam ac lobortis justo. Aliquam vestibulum
+            purus vitae lacus condimentum congue. Ut tincidunt tempus euismod.
+            Aliquam felis enim, luctus eu pharetra elementum, rutrum a justo.
+            Nunc pulvinar purus luctus tellus blandit, non blandit dui
+            fermentum. Proin non suscipit felis. Ut at nibh pharetra,
+            sollicitudin quam eget, maximus ex. Praesent laoreet pretium leo,
+            vel bibendum eros euismod accumsan.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ante
+            iaculis, euismod leo at, congue mi. Fusce ut lorem rutrum, placerat
+            ligula nec, tempus tellus. Donec fermentum sem sit amet ante cursus,
+            vitae scelerisque elit dictum. Suspendisse lectus nibh, eleifend
+            ultrices molestie at, mollis ullamcorper turpis. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Suspendisse sem lectus,
+            egestas ut lectus sit amet, convallis feugiat leo. Sed luctus
+            iaculis lorem et interdum. Nam ac lobortis justo. Aliquam vestibulum
+            purus vitae lacus condimentum congue. Ut tincidunt tempus euismod.
+            Aliquam felis enim, luctus eu pharetra elementum, rutrum a justo.
+            Nunc pulvinar purus luctus tellus blandit, non blandit dui
+            fermentum. Proin non suscipit felis. Ut at nibh pharetra,
+            sollicitudin quam eget, maximus ex. Praesent laoreet pretium leo,
+            vel bibendum eros euismod accumsan.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ante
+            iaculis, euismod leo at, congue mi. Fusce ut lorem rutrum, placerat
+            ligula nec, tempus tellus. Donec fermentum sem sit amet ante cursus,
+            vitae scelerisque elit dictum. Suspendisse lectus nibh, eleifend
+            ultrices molestie at, mollis ullamcorper turpis. Lorem ipsum dolor
+            sit amet, consectetur adipiscing elit. Suspendisse sem lectus,
+            egestas ut lectus sit amet, convallis feugiat leo. Sed luctus
+            iaculis lorem et interdum. Nam ac lobortis justo. Aliquam vestibulum
+            purus vitae lacus condimentum congue. Ut tincidunt tempus euismod.
+            Aliquam felis enim, luctus eu pharetra elementum, rutrum a justo.
+            Nunc pulvinar purus luctus tellus blandit, non blandit dui
+            fermentum. Proin non suscipit felis. Ut at nibh pharetra,
+            sollicitudin quam eget, maximus ex. Praesent laoreet pretium leo,
+            vel bibendum eros euismod accumsan.
+          </p>
+        </main>
+        <footer>By Daniel DR</footer>
+      </div>
+    </div>
+  );
+}
+
+export { App };
