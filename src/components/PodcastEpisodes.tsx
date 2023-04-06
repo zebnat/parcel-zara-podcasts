09334c705f11c2adc79be5dd9288348ab26ccca0
Daniel DR <contacto@danieldev.es>
2023-04-07 01:02:00 +0200

    feat: added dummy  PodcastEpisodes visual component

---
+++ src/components/PodcastEpisodes.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export function PodcastEpisodes() {
+  return (
+    <div>
+      <Link to="/podcast/74927349237/episode/43827943">Episode 43827943</Link>
+      <Link to="/podcast/74927349237/episode/123213">Episode 123213</Link>
+    </div>
+  );
+}
