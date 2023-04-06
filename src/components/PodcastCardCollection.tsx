9e15d4462d955cbea176dbe96961f132b447964d
Daniel DR <contacto@danieldev.es>
2023-04-06 22:25:56 +0200

    feat: podcastCardCollection now filters

--- src/components/PodcastCardCollection.tsx
+++ src/components/PodcastCardCollection.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { Podcast } from "../domain/Podcast";
 import { PodcastCard } from "./PodcastCard";
+import { FilterBox } from "./FilterBox";
+import { usePodcastCollectionFilter } from "../application/hooks/usePodcastCollectionFilter";
 
 export interface PodcastCardCollectionProps {
   items: Podcast[];
 }
 
 export function PodcastCardCollection(props: PodcastCardCollectionProps) {
-  const { items } = props;
+  const { filtered, onFilterChange } = usePodcastCollectionFilter(props.items);
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
-      {items.map((podcast) => (
-        <PodcastCard
-          id={podcast.id}
-          thumbnail={podcast.thumbnailUrl}
-          title={podcast.name}
-          author={podcast.author}
-        />
-      ))}
-    </div>
+    <>
+      <FilterBox
+        onFilterChange={onFilterChange}
+        total={filtered.length}
+        placeholder="Filter podcasts..."
+      />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+        {filtered.map((podcast) => (
+          <PodcastCard
+            id={podcast.id}
+            thumbnail={podcast.thumbnailUrl}
+            title={podcast.name}
+            author={podcast.author}
+          />
+        ))}
+      </div>
+    </>
   );
 }
