2988ae4ac8e9cdc2b1ef2dcb6046bd4105cd28e1
Daniel DR <contacto@danieldev.es>
2023-04-06 23:55:15 +0200

    feat: added router support to our app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,30 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
-import { App } from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home, PodcastDetails, RouteError } from "./components/pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <RouteError />,
+  },
+  {
+    path: "/podcast/:podcastId",
+    element: <PodcastDetails />,
+    children: [
+      {
+        path: "/podcast/:podcastId/episode/:episodeId",
+        element: <PodcastDetails />,
+      },
+    ],
+  },
+]);
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
