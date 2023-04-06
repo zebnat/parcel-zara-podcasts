bb5502aed69978afb18133ee6ee1394c4b23a5fa
Daniel DR <contacto@danieldev.es>
2023-04-07 01:01:29 +0200

    feat: added PodcastPresentation component
    it links to the podcast id page so you can return from the episode page

---
+++ src/components/PodcastPresentation.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface PodcastPresentationProps {
+  id: string;
+  name: string;
+  author: string;
+  description: string;
+  thumbnailUrl: string;
+}
+
+export function PodcastPresentation(props: PodcastPresentationProps) {
+  const { name, author, thumbnailUrl, description, id } = props;
+
+  return (
+    <Link
+      to={`/podcast/${id}`}
+      className="rounded-sm box-shadow shadow-[-1px_3px_3px_0px_rgba(0,0,0,0.3)]
+    w-full sm:w-1/3 p-4 flex flex-col gap-6 justify-center"
+    >
+      <img
+        className="rounded-md m-auto"
+        alt={name}
+        src={thumbnailUrl}
+        width="170"
+        height="170"
+      />
+      <div className="flex flex-col gap-1">
+        <span className="capitalize font-bold text-xl text-left">{name}</span>
+        <span className="capitalize italic text-gray-500 text-lg text-left">
+          By {author}
+        </span>
+      </div>
+
+      <div className="flex flex-col">
+        <span className="font-bold">Description:</span>
+        <p className="italic">{description}</p>
+      </div>
+    </Link>
+  );
+}
