50a6b80b380e01ea49e794c2a04b2d278b571927
Daniel DR <contacto@danieldev.es>
2023-04-06 23:51:25 +0200

    feat: added Layout component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react";
+import { StatusIndicator } from "./StatusIndicator";
+import { Link } from "react-router-dom";
+
+interface LayoutProps {
+  children?: ReactNode;
+}
+
+export function Layout(props: LayoutProps) {
+  return (
+    <div className="bg-gray-100 text-black px-10 py-5 min-h-screen">
+      <div className="w-full xl:w-3/4 m-auto flex flex-col">
+        <header className="flex justify-between">
+          <Link to="/" className="text-blue-500 text-2xl font-bold">
+            Podcaster
+          </Link>
+          <StatusIndicator loading={true} />
+        </header>
+        <main className="my-10 grow">{props.children}</main>
+        <footer>By Daniel DR</footer>
+      </div>
+    </div>
+  );
+}
