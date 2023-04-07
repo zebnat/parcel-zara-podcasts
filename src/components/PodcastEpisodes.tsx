2838f6088136ffb3e5093e3e6257c18188f23d0a
Daniel DR <contacto@danieldev.es>
2023-04-07 15:36:22 +0200

    feat: PodcastEpisodes visual component with hardcoded props

--- src/components/PodcastEpisodes.tsx
+++ src/components/PodcastEpisodes.tsx
@@ -1,11 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { PodcastEpisode } from "../domain/PodcastEpisode";
 
-export function PodcastEpisodes() {
+interface PodcastEpisodesProps {
+  podcastId: string;
+  items: PodcastEpisode[];
+}
+
+export function PodcastEpisodes(props: PodcastEpisodesProps) {
+  const { podcastId, items } = props;
+
+  const formatDate = (date: Date) => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    return `${day}-${month}-${year}`;
+  };
+
+  const trClassNames = `border border-y-0`;
   return (
-    <div>
-      <Link to="/podcast/74927349237/episode/43827943">Episode 43827943</Link>
-      <Link to="/podcast/74927349237/episode/123213">Episode 123213</Link>
+    <div className="grow">
+      <div className="p-2 mb-4 box-shadow shadow-[0px_3px_6px_2px_rgba(0,0,0,0.2)] font-bold ">
+        Episodes: {items.length}
+      </div>
+      <div className="p-2 mb-4 box-shadow shadow-[0px_3px_6px_2px_rgba(0,0,0,0.2)] flex">
+        <table className="table-auto grow">
+          <thead>
+            <tr className="font-bold text-left">
+              <th>Title</th>
+              <th>Date</th>
+              <th>Duration</th>
+            </tr>
+          </thead>
+          <tbody className="border">
+            {items.map((ep: PodcastEpisode, index: number) => (
+              <tr
+                key={ep.id}
+                className={
+                  trClassNames +
+                  `${index % 2 == 0 ? " bg-blue-100 saturate-75" : ""}`
+                }
+              >
+                <td className="p-2">
+                  <Link
+                    to={`/podcast/${podcastId}/episode/${ep.id}`}
+                    className="text-blue-700"
+                  >
+                    {ep.name}
+                  </Link>
+                </td>
+                <td>{formatDate(ep.date)}</td>
+                <td>{ep.duration}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- src/components/pages/PodcastDetails.tsx
+++ src/components/pages/PodcastDetails.tsx
@@ -3,11 +3,49 @@ import { Layout } from "../Layout";
 import { Outlet, useParams } from "react-router-dom";
 import { PodcastPresentation } from "../PodcastPresentation";
 import { PodcastEpisodes } from "../PodcastEpisodes";
+import { PodcastEpisode } from "../../domain/PodcastEpisode";
 
 interface PodcastDetailsRouteParams {
   podcastId: string;
   episodeId?: string;
 }
+
+// todo: this should come from api
+const dumbEpisodes: PodcastEpisode[] = [
+  {
+    id: "wadwdwa",
+    date: new Date(),
+    description: "This is a nice episode",
+    duration: "15:14",
+    episodeUrl: "",
+    name: "Some episode",
+  },
+  {
+    id: "wadwdwa",
+    date: new Date(),
+    description: "This is a nice episode",
+    duration: "15:41",
+    episodeUrl: "",
+    name: "Some episode",
+  },
+  {
+    id: "wadwdwa42",
+    date: new Date(),
+    description: "This is a nice episode 2",
+    duration: "20:00",
+    episodeUrl: "",
+    name: "Some episode 2",
+  },
+  {
+    id: "wadwdwa623",
+    date: new Date(),
+    description: "This is a nice episode 3",
+    duration: "10:00",
+    episodeUrl: "",
+    name: "Some episode 3",
+  },
+];
+
 export function PodcastDetails() {
   const { podcastId, episodeId } = useParams();
 
@@ -21,7 +59,12 @@ export function PodcastDetails() {
           description="This is the description"
           thumbnailUrl="https://is2-ssl.mzstatic.com/image/thumb/Podcasts125/v4/7b/cf/f6/7bcff6bb-5f99-6c2f-c6c5-3a9799f3df21/mza_8544742664200824246.jpg/170x170bb.png"
         />
-        {!episodeId && <PodcastEpisodes />}
+        {!episodeId && (
+          <PodcastEpisodes
+            podcastId={podcastId as string}
+            items={dumbEpisodes}
+          />
+        )}
         <Outlet />
       </div>
     </Layout>
