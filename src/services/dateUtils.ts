a0ca22de794e7747d8683ef670bdf8f8bbe1b48e
Daniel DR <contacto@danieldev.es>
2023-04-08 00:04:09 +0200

    feat: added new date utility function

---
+++ src/services/dateUtils.ts
@@ -0,0 +1,14 @@
+export function millisecondsToHourMinute(milliseconds: number) {
+  let seconds = Math.floor(milliseconds / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+
+  minutes = minutes % 60;
+
+  // Ensure two digits are always displayed
+  let hoursStr = hours.toString().padStart(2, "0");
+  let minutesStr = minutes.toString().padStart(2, "0");
+  let secondsStr = seconds.toString().padStart(2, "0");
+
+  return `${hoursStr}:${minutesStr}`;
+}
