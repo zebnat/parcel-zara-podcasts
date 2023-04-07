637f0e7bea34008394ad251b812ac39b0b9a95fb
Daniel DR <contacto@danieldev.es>
2023-04-07 14:35:12 +0200

    fix: improved Podcastcard and wrapper styling

--- src/components/PodcastCard.tsx
+++ src/components/PodcastCard.tsx
@@ -15,20 +15,25 @@ function PodcastCard(props: PodcastCardProps) {
   return (
     <Link to={podcastUrl}>
       <figure
-        className="p-3 bg-white border border-gray-400 box-shadow 
-                    flex flex-col justify-center items-center shadow-[-1px_3px_3px_0px_rgba(0,0,0,0.3)]"
+        className="p-3 bg-white border border-gray-400 box-shadow min-h-[100px]
+                    shadow-[-1px_3px_3px_0px_rgba(0,0,0,0.3)] mb-20"
       >
-        <img
-          className="rounded-full"
-          alt={title}
-          src={thumbnail}
-          width="170"
-          height="170"
-        />
-        <figcaption role="caption" className="uppercase font-bold text-md">
-          {title}
-        </figcaption>
-        <p className="text-gray-600">Author: {author}</p>
+        <div className="flex flex-col justify-center items-center pt-20 relative">
+          <img
+            className="rounded-full absolute -top-[80px]"
+            alt={title}
+            src={thumbnail}
+            width="160"
+            height="160"
+          />
+          <figcaption
+            role="caption"
+            className="uppercase font-bold text-md text-center"
+          >
+            {title}
+          </figcaption>
+          <p className="text-gray-600">Author: {author}</p>
+        </div>
       </figure>
     </Link>
   );
