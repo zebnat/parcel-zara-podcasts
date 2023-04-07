3e614db753e3656223393b51a21ebf758e63195c
Daniel DR <contacto@danieldev.es>
2023-04-07 18:15:09 +0200

    fix: removed strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -21,8 +21,4 @@ const router = createBrowserRouter([
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
