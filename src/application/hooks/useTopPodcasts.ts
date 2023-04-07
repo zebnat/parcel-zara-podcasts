3a0b84abcf662d5723328e7c2f37d3fb462b705d
Daniel DR <contacto@danieldev.es>
2023-04-07 20:05:10 +0200

    fix: add cache capabilities to our repository by using composition

--- src/application/hooks/useTopPodcasts.ts
+++ src/application/hooks/useTopPodcasts.ts
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import { Podcast } from "../../domain/Podcast";
 import { ItunesApi, ItunesRepositoryImpl } from "../../services/Itunesapi";
+import { ItunesApiStorageAdapter } from "../../services/ItunesApiStorageAdapter";
+import { ItunesRepository } from "../../domain/ItunesRepository";
 
 export function useTopPodcasts(): Podcast[] {
   const [podcasts, setPodcasts] = useState<Podcast[]>([]);
-  const repo = new ItunesRepositoryImpl(new ItunesApi());
+
+  const repo: ItunesRepository = new ItunesApiStorageAdapter(
+    new ItunesRepositoryImpl(new ItunesApi())
+  );
 
   useEffect(() => {
     (async function () {
