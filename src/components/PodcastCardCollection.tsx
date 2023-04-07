befb55156ca84eeb2a086beb972686d7e93b875b
Daniel DR <contacto@danieldev.es>
2023-04-07 18:36:53 +0200

    fix: CardCollection show a message when we have 0 podcasts

--- src/components/PodcastCardCollection.tsx
+++ src/components/PodcastCardCollection.tsx
@@ -18,9 +18,15 @@ export function PodcastCardCollection(props: PodcastCardCollectionProps) {
         total={filtered.length}
         placeholder="Filter podcasts..."
       />
+      {filtered.length === 0 && (
+        <div className="text-center text-xl text-gray-500">
+          Wait until we get some podcasts...
+        </div>
+      )}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 pt-20">
         {filtered.map((podcast) => (
           <PodcastCard
+            key={podcast.id}
             id={podcast.id}
             thumbnail={podcast.thumbnailUrl}
             title={podcast.name}
